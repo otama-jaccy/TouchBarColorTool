4b86c4b6a9f8b44f62b1976ff0aefddb18f2f6e9
Funakoshi Minto <funamin310@gmail.com>
2019-06-14 12:47:51 +0900

    Initial Commit

---
+++ TouchBarColorTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarColorTool
+//
+//  Created by Funakoshi Minto on 2019/06/14.
+//  Copyright © 2019年 Funakoshi Minto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarColorTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarColorTool
+//
+//  Created by Funakoshi Minto on 2019/06/14.
+//  Copyright © 2019年 Funakoshi Minto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
